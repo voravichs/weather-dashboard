f2af8f7173ae510cee55bd7ec70209555c8bbccb
VoravichS <vsilapac@gmail.com>
2022-08-01 18:49:43 -0500

    added forecast cards, convert old coordinates to geolocated coords from API, added function to clear search results on new search

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,15 @@ var searchFormEl = $('#search-form');
 var searchTextEl = $('#form-text');
 var searchHistoryEl = $('#search-history');
 var searchResultEl = $('#search-result');
-
-var cityList = ['chicago','new york','boston','denver','salt lake city',
-                'austin','phoenix','seattle','orlando','san francisco'];
-var cityCoordsList = [[41.85, -87.65],[40.7143, -74.006]];
-var currentCityCoords = [];
+var forecastCardsEl = $('#forecast-cards');
 
 var apiURL = 'https://api.openweathermap.org/data/2.5/onecall?';
-var apiExcludeQuery = '&exclude=hourly,daily';
+var apiGeoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=';
+var apiImgURL = 'http://openweathermap.org/img/wn/'
+var apiExcludeQuery = '&exclude=minutely,hourly';
 var apiUnits = '&units=imperial'
 var apiKey = '&appid=4f6a3fea6209de90cad619a8c1893016';
+var today = moment().format('l');
 
 // Handle submitting the form once a proper city is entered
 searchFormEl.on('submit', handleSubmit);
@@ -27,29 +26,25 @@ function handleSubmit (event) {
 
 // Check if city is valid in list of cities, then get the coords
 function searchCity(searchInput) {
-    // sanitize the input
+    // sanitize the input and build a geolocator URL
     searchInput = searchInput.toLowerCase().trim();
-    
-    // check if input is in list of cities
-    var foundCity = false;
-    for (let i = 0; i < cityList.length; i++) {
-        if (searchInput == cityList[i]) {
-            foundCity = true;
-            currentCityCoords = cityCoordsList[i];
-        }
-    }
+    var geoURL = apiGeoURL + searchInput + '&limit=1' + apiKey;
 
-    // if the city has been found, retrieve coordinates and build the query
-    if (foundCity) {
-        var lat = 'lat=' + currentCityCoords[0];
-        var lon = 'lon=' + currentCityCoords[1];
-        var cityQuery = lat + '&' + lon;
-        searchInput = capitalize(searchInput);
-        renderSearchResults(cityQuery, searchInput);
-    } else {
-        searchTextEl.text('Please enter a valid city name!');
-        return;
-    }
+    // Search for city given the input, take the first search result
+    fetch(geoURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            if (!data[0]) {
+                searchTextEl.text('Please enter a valid city name!');
+                return;
+            }
+            var lat = 'lat=' + data[0].lat;
+            var lon = 'lon=' + data[0].lon;
+            var cityQuery = lat + '&' + lon;
+            renderSearchResults(cityQuery, capitalize(searchInput));
+        })
 }
 
 // Retrieve API elements and visually add elements to page based on search query
@@ -66,25 +61,49 @@ function renderSearchResults(cityQuery, cityName) {
         })
         .then(function (data) {
             // Write search history
-            searchHistoryEl.prepend('<p class="rounded p-2 mb-2 custom-search-results">'+  cityName +'</p>');
+            searchHistoryEl.prepend('<p class="rounded p-2 mb-2 custom-search-results">'+  cityName + ' (' + today + ') ' + '</p>');
             
             // Write Search Result to Box
-            var todayWeatherList = ['Temp: ' + data.current.temp + ' °F', 'Wind Speed: '+ data.current.humidity + ' MPH', 'Humidity: ' + data.current.wind_speed + '%'];
-            searchResultEl.append('<h3 class="mb-3">'+ cityName +'</h3>');
+            var todayWeatherList = ['Temp: ' + data.current.temp + ' °F', 'Wind Speed: '+ data.current.wind_speed + ' MPH', 'Humidity: ' + data.current.humidity + '%'];
+            var weatherIconURL = apiImgURL + data.current.weather[0].icon + '.png';
+            var weatherIconImg = '<img src="'+ weatherIconURL +'" alt="'+ data.current.weather[0].description +'">'
+            searchResultEl.append('<h3 class="text-white fw-bolder mb-3">'+ cityName + ' (' + today + ') ' + weatherIconImg + '</h3>');
             for (let i = 0; i < todayWeatherList.length; i++) {
-                searchResultEl.append('<p class="ms-3">'+ todayWeatherList[i] +'</p>');
+                searchResultEl.append('<p class="text-white ms-3">'+ todayWeatherList[i] +'</p>');
             }
-            searchResultEl.append('<p class="ms-3">UV Index: <span class="rounded bg-success text-white px-3 py-2">' + data.current.uvi + '</span> </p> ');
-            //console.log(data.current);
-            //console.log(data.current.temp);
-            //console.log(data.current.humidity);
-            //console.log(data.current.wind_speed);
-            //console.log(data.current.uvi);
+            // check UV Index, assign color
+            var uvColor = 'bg-success'; 
+            var uvi = data.current.uvi;
+            if (uvi >= 8) {
+                uvColor = 'bg-danger';
+            } else if (uvi >= 6) {
+                uvColor = 'custom-bg-orange';
+            } else if (uvi >= 3) {
+                uvColor = 'bg-warning';
+            }
+            searchResultEl.append('<p class="text-white ms-3">UV Index: <span class="fw-bolder rounded ' + uvColor + ' text-white px-3 py-2">' + uvi + '</span> </p> ');
+            
+            // Write 5 day forecast to cards
+            // data.daily[1] to data.daily[6] is the next 5 days
+            for (let i = 1; i < 6; i++) {
+                var currDay = moment.unix(data.daily[i].dt).format('l');
+                var currIconURL = apiImgURL + data.daily[i].weather[0].icon + '@4x.png';
+                forecastCardsEl.append(
+                    '<div class="col card p-2 m-2 text-white custom-gradient-card"> \n' +
+                      '<h4>' + currDay + '</h4>' +
+                      '<img src="'+ currIconURL +'" alt="'+ data.daily[i].weather[0].description +'">' +
+                      '<p> Temp: ' + data.daily[i].temp.day + ' °F </p>' +
+                      '<p> Wind: ' + data.daily[i].wind_speed + ' MPH </p>' +
+                      '<p> Humidity: ' + data.daily[i].humidity + '% </p>' +
+                    '</div>'
+                )
+            }
         })
 }
 
 function removePreviousSearch() {
-    searchResultEl.innerHTML = '';
+    searchResultEl[0].innerHTML = '';
+    forecastCardsEl[0].innerHTML = '';
 }
 
 // Credit https://flexiple.com/javascript-capitalize-first-letter/ for implementation
